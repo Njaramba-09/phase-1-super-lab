06d190055e0d81caa272eab996797489c3188a39
brendanjaramba <njarambabrenda5@gmail.com>
2025-06-29 09:26:18 +0300

    firstcommit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your classes here
+// Parent class
+class Tree {
+  constructor(species) {
+    this.species = species;
+  }
+
+  static definition() {
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.";
+  }
+}
+
+// Deciduous class extending Tree
+class Deciduous extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+  }
+
+  static definition() {
+    return super.definition() + " Deciduous trees shed their leaves annually.";
+  }
+}
+
+// Evergreen class extending Tree
+class Evergreen extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+  }
+
+  static definition() {
+    return super.definition() + " Evergreens keep their leaves all year round.";
+  }
+}
